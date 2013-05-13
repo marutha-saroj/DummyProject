8d66074b79a1a7e6a7fbd963960eaa94cf364a83
marutha_saroj <marutha.saroj@gmail.com>
2013-05-13 17:28:45 +0530

    Added new Method

--- src/com/TestClass.java
+++ src/com/TestClass.java
@@ -3,7 +3,10 @@ public class TestClass {
 	  public static void main() {
 	    System.out.println("Hello World!!!!!!!!!!!!!!!");
 	  }
-	  public void branch_b1() {
+	  public void branch_b12() {
+	    System.out.println("Hello World!!!!!!!!!!!!!!!");
+	  }
+	   public void branch_b2() {
 	    System.out.println("Hello World!!!!!!!!!!!!!!!");
 	  }
      
