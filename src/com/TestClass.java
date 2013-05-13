329c3a34bb8f6fa82b08d4366cb6024c5b5c7a5a
marutha_saroj <marutha.saroj@gmail.com>
2013-05-13 17:21:04 +0530

    Added branch_b1 method

--- src/com/TestClass.java
+++ src/com/TestClass.java
@@ -3,6 +3,9 @@ public class TestClass {
 	  public static void main() {
 	    System.out.println("Hello World!!!!!!!!!!!!!!!");
 	  }
+	  public void branch_b1() {
+	    System.out.println("Hello World!!!!!!!!!!!!!!!");
+	  }
      
 	}
 
